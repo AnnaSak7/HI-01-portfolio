1f8ccda72c82bf5a07fd829607a3e0e75069dee4
Anna Sakurai <zenasak@gmail.com>
2021-09-24 09:34:51 +0200

    add media query -index & contact page. take out gallery page.

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,26 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+// function toggleState() {
+//   document.querySelector(".toggle-me").classList.toggle("active");
+// }
+
+function createIcons(icons) {
+  let div = document.getElementById("sm");
+  div.innerHTML += `<div class="social-media__img">
+      <a class="icon" href="${icons.title}"
+        ><i class="${icons.name}"></i
+      ></a>
+    </div>`;
+}
+function addIcons() {
+  fetch("../data/data.json")
+    .then((res) => res.json)
+    .then((data) => {
+      console.log("data : ", data);
+      for (let i = 0; i < data.length; i++) {
+        createIcons(data[i]);
+      }
+    });
+}
