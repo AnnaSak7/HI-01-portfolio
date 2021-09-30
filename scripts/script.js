a1eb68afacbce8bd0c2ddc74e48a8a1a1efef8a9
Anna Sakurai <zenasak@gmail.com>
2021-09-30 11:46:36 +0200

    add cv-download button - cv page. change font family - all. change text using svg - cv page.

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,17 @@
 //nav-bar
-const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".navbar");
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.navbar');
 
-menuIcon.addEventListener("click", () => {
-  navbar.classList.toggle("change");
+menuIcon.addEventListener('click', () => {
+  navbar.classList.toggle('change');
 });
-
-// function toggleState() {
-//   document.querySelector(".toggle-me").classList.toggle("active");
-// }
 
 function getTag(id) {
   return document.getElementById(id);
 }
 
 function createIcons(icons) {
-  let div = getTag("sm");
+  let div = getTag('sm');
   div.innerHTML += `<div class="social-media__img">
       <a class="icon" href="${icons.url}""
         ><i class="${icons.class}""></i
@@ -23,10 +19,10 @@ function createIcons(icons) {
     </div>`;
 }
 function addIcons() {
-  fetch("../data/data.json")
+  fetch('../data/data.json')
     .then((res) => res.json())
     .then((data) => {
-      console.log("data : ", data);
+      console.log('data : ', data);
       for (let i = 0; i < data.length; i++) {
         createIcons(data[i]);
       }
