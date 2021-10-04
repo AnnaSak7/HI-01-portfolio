f62bb6b4433602d873814cb945dffbbfc5cac04f
Anna Sakurai <zenasak@gmail.com>
2021-10-04 16:12:42 +0200

    fix responsiveness (mainly info box and download button)  -cv page

--- scripts/script.js
+++ scripts/script.js
@@ -7,52 +7,52 @@ menuIcon.addEventListener('click', () => {
 });
 
 ///////////////Intersection Observer /////////////////////////
-const card = document.querySelector('.education');
-const cards = document.querySelectorAll('.info-container');
+// const card = document.querySelector('.education');
+// const cards = document.querySelectorAll('.info-container');
 
-const option = {
-  root: null, // it is the viewport
-  threshold: 0.25,
-  rootMargin: '-150px',
-};
+// const option = {
+//   root: null, // it is the viewport
+//   threshold: 0.25,
+//   rootMargin: '-150px',
+// };
 
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return; //'return' ends the function immediately
-    }
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return; //'return' ends the function immediately
+//     }
 
-    console.log(entry.target);
-    entry.target.classList.toggle('visible');
-    observer.unobserve(entry.target);
-  });
-}, option);
+//     console.log(entry.target);
+//     entry.target.classList.toggle('visible');
+//     observer.unobserve(entry.target);
+//   });
+// }, option);
 
-cards.forEach((card) => {
-  observer.observe(card);
-});
+// cards.forEach((card) => {
+//   observer.observe(card);
+// });
 
-//observer.observe(card);
+// //observer.observe(card);
 
-///////////// adding social media icons //////////////////////
-function createIcons(icons) {
-  let div = getTag('sm');
-  div.innerHTML += `<div class="social-media__img">
-      <a class="icon" href="${icons.url}""
-        ><i class="${icons.class}""></i
-      ></a>
-    </div>`;
-}
-function addIcons() {
-  fetch('../data/data.json')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log('data : ', data);
-      for (let i = 0; i < data.length; i++) {
-        createIcons(data[i]);
-      }
-    });
-}
+// ///////////// adding social media icons //////////////////////
+// function createIcons(icons) {
+//   let div = getTag('sm');
+//   div.innerHTML += `<div class="social-media__img">
+//       <a class="icon" href="${icons.url}""
+//         ><i class="${icons.class}""></i
+//       ></a>
+//     </div>`;
+// }
+// function addIcons() {
+//   fetch('../data/data.json')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log('data : ', data);
+//       for (let i = 0; i < data.length; i++) {
+//         createIcons(data[i]);
+//       }
+//     });
+// }
 //////////////////////////////////////////////////////////////
 
 function getTag(id) {
